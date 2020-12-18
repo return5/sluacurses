71905bbb9c4aa563190f85c04374a1468d0c1653
return5 <armoredmoose@gmail.com>
2020-12-18 00:54:04 -0500

    fixed typo in example

--- examples/simple_example.lua
+++ examples/simple_example.lua
@@ -7,7 +7,7 @@ local Curses = require("sluacurses")  --load sluacurses into program
 
 initscr()                 --start ncurses
 noecho()                  --dont display keystrokes to screen
-curs(0)                   --dont display cursor
+curs_set(0)               --dont display cursor
 printw("hello world!\n")  --print to stdscr at currentl cursor location
 local c = getch()         --get char from user input
 printw("\nc is: ")
